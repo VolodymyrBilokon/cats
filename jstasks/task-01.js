12d4255d7478f6803e827204556043bfd03a48fe
VolodymyrBilokon <154451582+VolodymyrBilokon@users.noreply.github.com>
2024-01-20 17:13:29 +0200

    Create task-01.js

---
+++ jstasks/task-01.js
@@ -0,0 +1,5 @@
+function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  // code goes here
+  var sumGoals = laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+  return sumGoals;
+}
