50e55b47b1e99b903ad134511617eda1e51731d8
VolodymyrBilokon <154451582+VolodymyrBilokon@users.noreply.github.com>
2024-01-21 18:38:05 +0200

    Update task-06.js

--- jstasks/task-06.js
+++ jstasks/task-06.js
@@ -1,1 +1,10 @@
+function lovefunc(flower1, flower2) {
+  if (
+    (flower1 % 2 == 0 && flower2 % 2 == 1) ||
+    (flower2 % 2 == 0 && flower1 % 2 == 1)
+  ) {
+    return true;
+  }
+  return false;
+}
 
